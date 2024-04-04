67efc0a6f2e2670290a5b93c3b81cf7e8136e821
Chinmay <chinmaybarik214@gmail.com>
2024-04-04 11:29:53 +0530

    working input field auth

--- vite/auth/src/App.jsx
+++ vite/auth/src/App.jsx
@@ -4,51 +4,53 @@ import './App.css'
 import axios from 'axios'
 
 function App() {
-  
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
   const [loginData, setLoginData] = useState(null)
 
-  async function autoLogin() {
+  async function loginUser() {
     try {
       const response = await axios.post(
         'http://172.16.94.178:1337/api/auth/local',
         {
-          identifier: 'testing',
-          password: 'Testing@123'
+          identifier: username,
+          password: password
         }
       )
       console.log(response.data)
-      
-      setLoginData(response.data)
-      localStorage.setItem('loginData',loginData)
 
-    } catch(error) {
+      setLoginData(response.data)
+      localStorage.setItem('loginData', loginData)
+
+    } catch (error) {
       console.log(error)
     }
   }
-
-  useEffect(()=>{
-    autoLogin()
-    return ()=>{}
-  },[]);
-
   return (
     <>
       {
         loginData
-        ? (
-          <>
-            <h1>Logged in as "{loginData.user.username}"</h1>
-            <button onClick={()=>{setLoginData(null)}}>Log Out</button>
-          </>
-        )
-        : (
-          <>
-            <h1>Logged Out</h1>
-            <form>
-              <input />
-            </form>
-          </>
-        )
+          ? (
+            <>
+              <h1>Logged in as "{loginData.user.username}"</h1>
+              <button onClick={() => { setLoginData(null) }}>Log Out</button>
+            </>
+          )
+          : (
+            <>
+              <h1>Logged Out</h1>
+              <form>
+                <input onChange={(e) => {
+                  setUsername(e.target.value)
+                }} />
+                <input onChange={(e) => {
+                  setPassword(e.target.value)
+                }} />
+                <button type='button' onClick={loginUser}>Sign in</button>
+              </form>
+            </>
+          )
       }
     </>
   )
